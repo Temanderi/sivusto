750dbb897e03baef2630cdb30fd3d40aa771976d
Temanderi <temanderi@gmail.com>
2026-06-01 16:02:46 +0300

    Update post.js

--- post.js
+++ post.js
@@ -1,18 +1,46 @@
-const params = new URLSearchParams(window.location.search);
-const slug = params.get("slug");
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <meta charset="UTF-8">
+    <title>Postaus</title>
 
-fetch("posts.json")
-  .then(r => r.json())
-  .then(posts => {
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 
-    const post = posts.find(p => p.slug === slug);
+<div class="container">
 
-    if (!post) {
-      document.body.innerHTML = "<h1>Postia ei löydy</h1>";
-      return;
-    }
+    <a href="index.html">← Takaisin</a>
 
-    document.getElementById("title").innerText = post.title;
-    document.getElementById("content").innerText = post.content;
+    <h1 id="title"></h1>
+    <p id="content"></p>
 
-  });
+    <!-- Kommentit otsikko -->
+    <div class="comment-header">Kommentit</div>
+
+    <!-- Disqus -->
+    <div id="disqus_thread"></div>
+
+</div>
+
+<!-- Disqus script -->
+<script id="dsq-count-scr" src="//taikatemppu-link.disqus.com/count.js" async></script>
+
+<script>
+var disqus_config = function () {
+    this.page.url = window.location.href;
+    this.page.identifier = window.location.href;
+};
+
+(function() {
+    var d = document, s = d.createElement('script');
+    s.src = 'https://taikatemppu-link.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+})();
+</script>
+
+<script src="post.js"></script>
+
+</body>
+</html>
